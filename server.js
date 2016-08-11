439f7fc90c1b2057fa2c814ca5b207707cf5ca0e
Jasper Francisco <jasper.francisco@gmail.com>
2016-08-11 08:25:30 +0000

    main server script now routes properly and serves public dir'

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 var express = require('express');
-
-// Import main-router.js for routing
-var mainRouter = require('./main-router.js');
+// var path = require('path');
 
 var app = express();
-
 var port = process.env.PORT || 8080;
+
+// Middlewares
+app.use('/public', express.static('public'));
+
+// Routing Import
+var mainRouter = require('./main-router.js');
 
 // Application Routing
 app.use('/', mainRouter);
 
-app.listen(port);
+app.listen(port, function(err) {
+    if (err) throw err;
+    
+    console.log("Server is running!");
+});
