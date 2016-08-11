a9f2b0bbe390d290f165a61aeb77b86825243a95
Jasper Francisco <jasper.francisco@gmail.com>
2016-08-11 08:23:37 +0000

    main router now serves an html at root, request header now routes

--- main-router.js
+++ main-router.js
@@ -1,15 +1,17 @@
 var express = require('express');
+var path = require('path');
 
 // FCC API projects (in order)
 var timestamp = require('./controllers/timestamp.js');
-//var requestHeader = require('./controllers/request-header.js');
+var requestHeader = require('./controllers/request.js');
 
 var mainRouter = express.Router();
 
 mainRouter.get('/', (req, res) => {
-    res.send('Hullo, world!');
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
+
 mainRouter.get('/timestamp', timestamp);
-//mainRouter.get('/request', requestHeader);
+mainRouter.get('/request', requestHeader);
 
 module.exports = mainRouter;
